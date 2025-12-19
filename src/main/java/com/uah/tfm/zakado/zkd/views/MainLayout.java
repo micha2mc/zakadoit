4d2bde9667c7af49596a9166cd5dd512035f5348
micha <micha2mc@gmail.com>
2025-12-19 15:58:21 +0100

    Ignorar carpeta generated

---
+++ src/main/java/com/uah/tfm/zakado/zkd/views/MainLayout.java
@@ -0,0 +1,49 @@
+package com.uah.tfm.zakado.zkd.views;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+@Route("")
+public class MainLayout extends AppLayout {
+
+    public MainLayout() {
+
+        createHeader();
+        createDrawer();
+    }
+
+    private void createHeader() {
+        H1 logo = new H1("ZAKADO IT");
+        logo.addClassNames(
+                LumoUtility.FontSize.LARGE,
+                LumoUtility.Margin.MEDIUM);
+
+        //String u = securityService.getAuthenticatedUser().getUsername();
+        Button logout = new Button("Log out ");// + u, e -> securityService.logout()); // <2>
+
+        var header = new HorizontalLayout(new DrawerToggle(), logo, logout);
+
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.expand(logo); // <4>
+        header.setWidthFull();
+        header.addClassNames(
+                LumoUtility.Padding.Vertical.NONE,
+                LumoUtility.Padding.Horizontal.MEDIUM);
+
+        addToNavbar(header);
+
+    }
+
+    private void createDrawer() {
+        /*addToDrawer(new VerticalLayout(
+                new RouterLink("List", ListView.class),
+                new RouterLink("Dashboard", DashboardView.class)
+        ));*/
+    }
+}
